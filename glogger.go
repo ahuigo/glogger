29542aa9f4fa55ba78e263e8e4c0e57e76c52e5d
yexuehui <yexuehui@momenta.ai>
2022-01-12 21:29:31 +0800

    feat: support console/s & trace

--- glogger.go
+++ glogger.go
@@ -2,10 +2,14 @@ package glogger
 
 import (
 	"encoding/json"
+	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+// Refer: go.uber.org/zap@v1.16.0/example_test.go
+// zapcore.NewTee( zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
 
 // Global logger
 var Glogger = GetLogger("root", zap.DebugLevel)
@@ -17,34 +21,48 @@ func SetGlogger(name string, level zapcore.Level) {
 
 func GetLogger(name string, level zapcore.Level) *zap.SugaredLogger {
 	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:    "time",
-		LevelKey:   "level",
-		NameKey:    "logger",
-		CallerKey:  "caller",
-		MessageKey: "msg",
-		// StacktraceKey: "stacktrace",
-		LineEnding:  zapcore.DefaultLineEnding,
-		EncodeLevel: zapcore.CapitalLevelEncoder,
-		EncodeTime:  zapcore.ISO8601TimeEncoder,
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.CapitalLevelEncoder,
+		EncodeTime:    zapcore.ISO8601TimeEncoder,
 		// EncodeDuration: zapcore.SecondsDurationEncoder,
 		EncodeDuration: zapcore.StringDurationEncoder,
 		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
 	// 设置日志级别
+	encoding := "console"
+	if os.Getenv("APP_ENV") != "" && os.Getenv("APP_ENV") != "dev" {
+		encoding = "json"
+
+	}
 	atom := zap.NewAtomicLevelAt(level)
 	config := zap.Config{
 		Level:            atom,
 		Development:      false,
-		Encoding:         "console",
+		Encoding:         encoding,
 		EncoderConfig:    encoderConfig,
 		InitialFields:    map[string]interface{}{},
 		OutputPaths:      []string{"stdout"},
 		ErrorOutputPaths: []string{"stderr"},
 	}
+	// option := make(zap.Option, 0)
 	loggerRaw, _ := config.Build()
-	logger := loggerRaw.Named(name).Sugar()
-	// logger.core.callerSkip += 2
+	options := []zap.Option{
+		zap.AddCaller(),
+		zap.AddCallerSkip(1),
+		// logger.core.callerSkip += 2
+		zap.AddStacktrace(zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl >= zapcore.ErrorLevel
+		})),
+		// zap.Development(),
+	}
+	logger := loggerRaw.WithOptions(options...).Named(name).Sugar()
 	return logger
 }
 
--- tests/logger_test.go
+++ tests/logger_test.go
@@ -3,33 +3,35 @@ package tests
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/ahuigo/glogger"
 	"go.uber.org/zap"
-    "fmt"
 )
 
-type User struct{
-    Name string 
-    Age int
+type User struct {
+	Name string
+	Age  int
 }
 
 func TestLogger(t *testing.T) {
-    fmt.Println("default log format:")
-    fmt.Println("datetime                	log_level project_name	code_path:line	<message>")
-    // this log is named root
+	fmt.Println("default log format:")
+	fmt.Println("datetime                	log_level project_name	code_path:line	<message>")
+	// this log is named root
 	glogger.Glogger.Debug("This is a debug log")
 
-    // get logger named with "proj"
-    logger:=glogger.GetLogger("proj", zap.InfoLevel)
-    logger.Info("This is a info log")
+	// get logger named with "proj"
+	logger := glogger.GetLogger("proj", zap.InfoLevel)
+	logger.Info("This is a info log")
+	logger.Info("This is a info log")
 	logger.Debug("This is a debug log") //not printed
 
-    user:=User{
-        Name:"name",
-        Age:1,
-    }
-    // print data log
-    fmt.Println("print data log")
+	user := User{
+		Name: "name",
+		Age:  1,
+	}
+	// print data log
 	glogger.Glogger.Debug(user)
+	glogger.Glogger.Error(user)
 	glogger.Glogger.Debug(glogger.JsonEncode(user))
 }
